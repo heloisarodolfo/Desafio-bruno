7126cbe4fb4f2885f5b0140af016121918a8d0bb
victorschool21 <132473229+victorschool21@users.noreply.github.com>
2023-05-05 22:44:25 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+import TelaA from './components/TelaA';
+import TelaB from './components/TelaB';
+import TelaC from './components/TelaC';
+
+import AssetExample from './components/TelaA';
+
+
+import { Card } from 'react-native-paper';
+
+export default function App() {
+  return (
+    <View >
+   
+    </View>
+  );
+}
